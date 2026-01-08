48acafda7bd64eab21b78ef12f98302e96ad912b
krijnlammers <krijn.lammers@gmail.com>
2026-01-08 23:00:13 +0100

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// minimal modal trigger
-document.querySelectorAll('[data-cta="intake"]').forEach(el=>{
-  el.addEventListener('click',e=>{
-    e.preventDefault();
-    window.location.href='mailto:intake@sentiodynamics.com';
-  });
-});
